cf3f1850203fa127d80482004263ae9b7e111a36
Jonas Kneifl <jonas.kneifl@itm.uni-stuttgart.de>
2024-06-13 16:23:24 +0200

    and another one

--- vindy/layers/__init__.py
+++ vindy/layers/__init__.py
@@ -1,2 +1,3 @@
-from .sindy_layer import SindyLayer
-from .vindy_layer import VindyLayer
+from . import sindy_layer, vindy_layer
+from sindy_layer import SindyLayer
+from vindy_layer import VindyLayer
--- vindy/libraries/__init__.py
+++ vindy/libraries/__init__.py
@@ -1,5 +1,6 @@
-from .exponential_library import ExponentialLibrary
-from .polynomial_library import PolynomialLibrary
-from .fourier_library import FourierLibrary
-from .force_library import ForceLibrary
-from .base_library import BaseLibrary
+from . import base_library, exponential_library, polynomial_library, fourier_library, force_library
+from exponential_library import ExponentialLibrary
+from polynomial_library import PolynomialLibrary
+from fourier_library import FourierLibrary
+from force_library import ForceLibrary
+from base_library import BaseLibrary
