43d8e7d27655974a501b7cf133a5100887a06b49
Jonas Kneifl <jonas.kneifl@itm.uni-stuttgart.de>
2024-06-13 16:18:17 +0200

    another try

--- vindy/__init__.py
+++ vindy/__init__.py
@@ -1,7 +1,7 @@
 # from vindy.networks import AutoencoderSindy, VAESindy, SindyNetwork
-import networks
-import layers
-import utils
-import libraries
-import callbacks
-from networks import AutoencoderSindy, VAESindy, SindyNetwork
+from . import networks
+from . import layers
+from . import utils
+from . import libraries
+from . import callbacks
+# from networks import AutoencoderSindy, VAESindy, SindyNetwork
