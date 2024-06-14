964ba564f9fce695d03c50be26b896424e0430c6
Jonas Kneifl <jonas.kneifl@itm.uni-stuttgart.de>
2024-06-14 10:32:29 +0200

    import statements on top level

--- vindy/__init__.py
+++ vindy/__init__.py
@@ -1,8 +1,1 @@
-# from vindy.networks import AutoencoderSindy, VAESindy, SindyNetwork
-# from . import callbacks
-# from . import layers
-# from . import libraries
-# from . import networks
-# from . import utils
-# from . import distributions
-# from networks import AutoencoderSindy, VAESindy, SindyNetwork
+from .networks import AutoencoderSindy, VAESindy, SindyNetwork
